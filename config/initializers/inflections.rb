6024dc1f4106ceb57ccccf8e21e259e5ebfcccd4
isaac-caliz <icaliz@infile.com>
2024-10-03 13:10:33 -0600

    con inflections y con simple_form

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -3,12 +3,18 @@
 # Add new inflection rules using the following format. Inflections
 # are locale specific, and you may define rules for as many different
 # locales as you wish. All of these examples are active by default:
-# ActiveSupport::Inflector.inflections(:en) do |inflect|
-#   inflect.plural /^(ox)$/i, "\\1en"
-#   inflect.singular /^(ox)en/i, "\\1"
-#   inflect.irregular "person", "people"
-#   inflect.uncountable %w( fish sheep )
-# end
+ActiveSupport::Inflector.inflections(:en) do |inflect|
+  #   inflect.plural /^(ox)$/i, "\\1en"
+  #   inflect.singular /^(ox)en/i, "\\1"
+  #   inflect.irregular "person", "people"
+  #   inflect.uncountable %w( fish sheep )
+  inflect.irregular "vendedor", "vendedores"
+  inflect.irregular "cliente", "clientes"
+  inflect.irregular "proveedor", "proveedores"
+  inflect.irregular "producto", "productos"
+  inflect.irregular "venta", "ventas"
+  inflect.irregular "venta_detalle", "ventas_detalles"
+end
 
 # These inflection rules are supported but not enabled by default:
 # ActiveSupport::Inflector.inflections(:en) do |inflect|
