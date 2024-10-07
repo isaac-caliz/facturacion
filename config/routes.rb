880dc3000ebe0ff95f721e14b9c5e10c17139947
isaac-caliz <icaliz@infile.com>
2024-10-07 15:32:53 -0600

    add login and style form

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :ventadetalles
   resources :ventas
   resources :vendedores
@@ -19,5 +20,5 @@ Rails.application.routes.draw do
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
  # Defines the root path route ("/")
- root "proveedores#index"
+ root "visitors#index"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_10_04_190249) do
+ActiveRecord::Schema[7.2].define(version: 2024_10_07_185039) do
   create_table "clientes", force: :cascade do |t|
     t.string "nombres"
     t.string "apellidos"
@@ -42,6 +42,18 @@ ActiveRecord::Schema[7.2].define(version: 2024_10_04_190249) do
     t.string "direccion"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   create_table "vendedores", force: :cascade do |t|
